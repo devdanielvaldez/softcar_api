c1192d4eec8f2e687fa392be6cd86fbe066cd9a5
Daniel <hello@daniel-valdez.com>
2023-01-21 20:40:57 -0400

    create class schemas

---
+++ src/schema/class.schema.ts
@@ -0,0 +1,30 @@
+import { Schema, model, ObjectId, Types } from 'mongoose';
+
+export interface IClass {
+    name: string;
+    state: boolean;
+    familiesId: ObjectId
+}
+
+const classSchema = new Schema<IClass>({
+    name: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: Boolean,
+        required: true
+    },
+    familiesId: {
+        type: Types.ObjectId,
+        ref: "Families"
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false,
+        collection: "Class"
+    }
+);
+
+export const Taxes = model<IClass>('Class', classSchema);
