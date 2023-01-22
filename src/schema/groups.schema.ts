a1c2e1198c71d5946f51e0a819755172b90ac715
Daniel <hello@daniel-valdez.com>
2023-01-21 20:43:02 -0400

    create groups schema

---
+++ src/schema/groups.schema.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from 'mongoose';
+
+export interface IGroups {
+    name: string;
+    preBooking: number;
+    state: boolean;
+}
+
+const groupsSchema = new Schema<IGroups>({
+    name: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: Boolean,
+        required: true
+    },
+    preBooking: {
+        type: Number,
+        required: true
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false,
+        collection: "Groups"
+    }
+);
+
+export const Taxes = model<IGroups>('Groups', groupsSchema);
